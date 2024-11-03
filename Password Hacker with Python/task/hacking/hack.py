3c9871c4a47b43e436e19be8416ed8190fe746e5
anton_stepanets <anton.stepanets15@gmail.com>
2024-11-03 16:33:45 +0100

    brute force

--- Password Hacker with Python/task/hacking/hack.py
+++ Password Hacker with Python/task/hacking/hack.py
@@ -1,25 +1,37 @@
 import sys
 import socket
+import itertools
+import string
 
 
 def main():
-    if len(sys.argv) != 4:
-        print("Usage: python hack.py <IP> <port> <message>")
+    if len(sys.argv) != 3:
+        print("Usage: python hack.py <IP> <port>")
         return
 
     ip = sys.argv[1]
     port = int(sys.argv[2])
-    message = sys.argv[3]
+
+    characters = string.ascii_lowercase + string.digits
 
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((ip, port))
 
-            s.sendall(message.encode())
+            for length in range(1, len(characters) + 1):
+                for password_tuple in itertools.product(characters, repeat=length):
+                    password = ''.join(password_tuple)
 
-            response = s.recv(1024).decode()
+                    s.sendall(password.encode())
 
-            print(response)
+                    response = s.recv(1024).decode()
+
+                    if response == "Connection success!":
+                        print(password)
+                        return
+                    elif response == "Too many attempts":
+                        print("Too many attempts. Aborting.")
+                        return
 
     except Exception as e:
         print(f"An error occurred: {e}")
